bd52521efc739824180ffabc087be324544bf4cf
DBuckley0126 <DannyBoy.0126>
2020-02-03 16:24:22 +0000

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,21 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+        <h1>Actors Page</h1>
+        { actors.map(({ name, movies }, index) =>
+          <div key={ index }>
+            { name }
+            <ul>
+            { movies.map((movie, index) => {
+                return (
+                  <li key={ index }>
+                    { movie }
+                  </li>
+                )
+            })}            
+            </ul>
+          </div>
+        )}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        { movies.map(({ title, time, genres }, index ) => 
+        <div key={index} className="movie" >
+          <h3>
+            { title } - { time } mins
+          </h3>
+          <ul>
+            {genres.map((genre, index) => {
+              return <li key={ index }>{genre}</li>;
+              }
+            )}
+          </ul>
+        </div>
+          )}
     </div>
   );
 };
